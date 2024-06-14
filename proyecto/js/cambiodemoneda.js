a61f3a33ed44065ea8d0b4004a4a02b5a5ce6700
juanfg97 <75532762+juanmanuelfg9@users.noreply.github.com>
2024-06-14 10:16:03 -0400

    Delete proyecto/js/cambiodemoneda.js

--- proyecto/js/cambiodemoneda.js
+++
@@ -1,8 +0,0 @@
-// Realiza una solicitud a la API para obtener datos sobre el dólar
-fetch("https://ve.dolarapi.com/v1/dolares/oficial")
-  .then(response => response.json()) // Convierte la respuesta a formato JSON
-  .then(data => {
-  const d = data.promedio;
-console.log(d);
-
-  });
