69e402be1e655c612d102a434330a1e560e5d9e1
juanfg97 <75532762+juanmanuelfg9@users.noreply.github.com>
2024-06-25 22:06:59 -0400

    Esto es un futuro  json
    Se encuentra fuera de la carpeta js y con extencion js xq la extension json no deja comentar, entonces provicionalmente por cuestiones de comodidad se queda asi, al final se modificara a json quitando todos los comentarios

---
+++ Libros.js
@@ -0,0 +1,393 @@
+// Libros
+[
+    // nuevos
+    {
+        id: "nuevo-01",
+        titulo: "Ciudad medialuna",
+        imagen: "img/Libros/new/ciudad-medialuna.avif",
+        categoria: {
+            nombre: "Nuevos",
+            id: "nuevo"
+        },
+    },
+    {
+        id: "nuevo-02",
+        titulo: "El color de las cosas invisibles",
+        imagen: "img/Libros/new/ciudad-medialuna.avif",
+        categoria: {
+            nombre: "Nuevos",
+            id: "nuevo"
+        },
+    },
+    {
+        id: "nuevo-03",
+        titulo: "El hijo olvidado",
+        imagen: "img/Libros/new/el-hijo-olvidado.avif",
+        categoria: {
+            nombre: "Nuevos",
+            id: "nuevo"
+        },
+    },
+    {
+        id: "nuevo-04",
+        titulo: "Rivales Divinos",
+        imagen: "img/Libros/new/rivales-divinos.avif",
+        categoria: {
+            nombre: "Nuevos",
+            id: "nuevo"
+        },
+    },
+    {
+        id: "nuevo-05",
+        titulo: "Si lo crees,lo creas",
+        imagen: "img/Libros/new/si-lo-crees_-lo-creas.avif",
+        categoria: {
+            nombre: "Nuevo",
+            id: "nuevo"
+        },
+    },
+    // Populares
+    {
+        id: "popular-01",
+        titulo: "Alas de hierro",
+        imagen: "img/Libros/populares/alas-de-hierro.avif",
+        categoria: {
+            nombre: "Popular",
+            id: "popular"
+        },
+    },
+    {
+        id: "popular-02",
+        titulo: "Catch the wind",
+        imagen: "img/Libros/populares/Catch the wind.jfif",
+        categoria: {
+            nombre: "Popular",
+            id: "popular"
+        },
+    },
+    {
+        id: "popular-03",
+        titulo: "Fuego y sangre",
+        imagen: "img/Libros/populares/fuego y sangre.avif",
+        categoria: {
+           nombre: "Popular",
+            id: "popular"
+        },
+      
+    },
+    {
+        id: "popular-04",
+        titulo: "Grounded for all eternity",
+        imagen: "img/Libros/populares/groundedforalleternity.jfif",
+        categoria: {
+           nombre: "Popular",
+            id: "popular"
+        },
+        
+    },
+    {
+        id: "popular-05",
+        titulo: "Habitos atomicos",
+        imagen: "img/Libros/populares/habitos-atomicos.avif",
+        categoria: {
+       nombre: "Popular",
+            id: "popular"
+        },
+        
+    },
+    {
+        id: "popular-06",
+        titulo: "Julia Quinn",
+        imagen: "img/Libros/populares/Julia Quinn.avif",
+        categoria: {
+           nombre: "Popular",
+            id: "popular"
+        },
+    },
+    {
+        id: "popular-07",
+        titulo: "La paciente silenciosa",
+        imagen: "img/Libros/populares/la-paciente-silenciosa.avif",
+        categoria: {
+            nombre: "Popular",
+            id: "popular"
+        },
+    },
+    {
+        id: "popular-08",
+        titulo: "Romper el hielo",
+        imagen: "img/Libros/populares/romper-el-hielo.avif",
+        categoria: {
+            nombre: "Popular",
+            id: "popular"
+        },
+     },
+    {
+        id: "popular-09",
+        titulo: "Tan poca vida",
+        imagen: "img/Libros/populares/tan-poca-vida.avif",
+        categoria: {
+          nombre: "Popular",
+            id: "popular"
+        },
+    },
+    {
+        id: "popular-10",
+        titulo: "The last daughther",
+        imagen: "img/Libros/populares/Thelastdaughther.jfif",
+        categoria: {
+          nombre: "Popular",
+            id: "popular"
+        },
+    },
+   
+    // Ciencia Ficcion
+    {
+        id: "cienciaf-01",
+        titulo: "ciner",
+        imagen: "img/Libros/cienciaficcion/ciner.avif",
+        categoria: {
+            nombre: "Cienciaf",
+            id: "cienciaf"
+        },
+    },
+    {
+        id: "cienciaf-02",
+        titulo: "Ciudad medialuna",
+        imagen: "img/Libros/cienciaficcion/ciudad-medialuna_1.avif",
+        categoria: {
+              nombre: "Cienciaf",
+            id: "cienciaf"
+        },
+    },
+    {
+        id: "cienciaf-03",
+        titulo: "Correr o morir",
+        imagen: "img/Libros/cienciaficcion/correr o morir.avif",
+        categoria: {
+             nombre: "Cienciaf",
+            id: "cienciaf"
+        },
+    },
+    {
+        id: "cienciaf-04",
+        titulo: "El ejercito de la reina",
+        imagen: "img/Libros/cienciaficcion/el ejercito del la reina.avif",
+        categoria: {
+            nombre: "Cienciaf",
+            id: "cienciaf"
+        },
+    },
+    {
+        id: "cienciaf-05",
+        titulo: "Fairest",
+        imagen: "img/Libros/cienciaficcion/fairest.avif",
+        categoria: {
+          nombre: "Cienciaf",
+            id: "cienciaf"
+        },
+    },
+    {
+        id: "cienciaf-06",
+        titulo: "Los juegos del Hambre",
+        imagen: "img/Libros/cienciaficcion/los juegos del hambre.avif",
+        categoria: {
+          nombre: "Cienciaf",
+            id: "cienciaf"
+        },
+    },
+  //Drama
+  {
+    id: "Drama-01",
+    titulo: "Casa de muñecas",
+    imagen: "img/Libros/drama/Casademuñecas.avif",
+    categoria: {
+      nombre: "Drama",
+        id: "drama"
+    },
+},
+{
+    id: "Drama-02",
+    titulo: "La campana de cristal",
+    imagen: "img/Libros/drama/Lacampanadecristal.avif",
+    categoria: {
+      nombre: "Drama",
+        id: "drama"
+    },
+},
+{
+    id: "Drama-03",
+    titulo: "La gaviota",
+    imagen: "img/Libros/drama/Lagaviota.avif",
+    categoria: {
+       nombre: "Drama",
+        id: "drama"
+    },
+},
+{
+    id: "Drama-04",
+    titulo: "Muerte de un viajante",
+    imagen: "img/Libros/drama/Muertedeunviajante.avif",
+    categoria: {
+     nombre: "Drama",
+        id: "drama"
+    },
+},
+{
+    id: "Drama-05",
+    titulo: "Tan poca vida",
+    imagen: "img/Libros/drama/tan-poca-vida.avif",
+    categoria: {
+      nombre: "Drama",
+        id: "drama"
+    },
+},
+//Fantasia
+
+{
+    id: "Fantasia-01",
+    titulo: "Alas de hierro",
+    imagen: "img/Libros/fantasia/alas-de-hierro.avif",
+    categoria: {
+      nombre: "Fantasia",
+        id: "fantasia"
+    },
+},
+{
+    id: "Fantasia-01",
+    titulo: "Alas de hierro",
+    imagen: "img/Libros/fantasia/alas-de-hierro.avif",
+    categoria: {
+      nombre: "Fantasia",
+        id: "fantasia"
+    },
+},
+{
+    id: "Fantasia-02",
+    titulo: "Aleacion de ley",
+    imagen: "img/Libros/fantasia/aleacion de ley.avif",
+    categoria: {
+      nombre: "Fantasia",
+        id: "fantasia"
+    },
+},
+{
+    id: "Fantasia-03",
+    titulo: "Bibliotecarios malvados",
+    imagen: "img/Libros/fantasia/bibliotecarios malvados.avif",
+    categoria: {
+      nombre: "Fantasia",
+        id: "fantasia"
+    },
+},
+{
+    id: "Fantasia-04",
+    titulo: "Brazales de duelo",
+    imagen: "img/Libros/fantasia/brazales de duelo.avif",
+    categoria: {
+      nombre: "Fantasia",
+        id: "fantasia"
+    },
+},
+{
+    id: "Fantasia-05",
+    titulo: "Catch the wind",
+    imagen: "img/Libros/fantasia/Catch the wind.jfif",
+    categoria: {
+      nombre: "Fantasia",
+        id: "fantasia"
+    },
+},
+{
+    id: "Fantasia-06",
+    titulo: "Citonica",
+    imagen: "img/Libros/fantasia/citonica.avif",
+    categoria: {
+      nombre: "Fantasia",
+        id: "fantasia"
+    },
+},
+
+{
+    id: "Fantasia-07",
+    titulo: "Fuego y sangre",
+    imagen: "img/Libros/fantasia/fuego y sangre.avif",
+    categoria: {
+      nombre: "Fantasia",
+        id: "fantasia"
+    },
+},
+{
+    id: "Misterio-01",
+    titulo: "El hijo olvidado",
+    imagen: "img/Libros/misterio/el-hijo-olvidado.avif",
+    categoria: {
+      nombre: "Misterio",
+        id: "misterio"
+    },
+},
+{
+    id: "Misterio-02",
+    titulo: "Grounded for all eternity",
+    imagen: "img/Libros/misterio/groundedforalleternity.jfif",
+    categoria: {
+      nombre: "Misterio",
+        id: "misterio"
+    },
+},
+{
+    id: "Misterio-03",
+    titulo: "The last daughther",
+    imagen: "img/Libros/misterio/Thelastdaughther.jfif",
+    categoria: {
+     nombre: "Misterio",
+        id: "misterio"
+    },
+},
+//Romance
+{
+    id: "Romance-01",
+    titulo: "Jaque mate al amor",
+    imagen: "img/Libros/romance/jaque mate al amor.avif",
+    categoria: {
+     nombre: "Romance",
+        id: "romance"
+    },
+},
+{
+    id: "Romance-02",
+    titulo: "La quimica del amor",
+    imagen: "img/Libros/romance/la quimica del amor.avif",
+    categoria: {
+     nombre: "Romance",
+        id: "romance"
+    },
+},
+{
+    id: "Romance-03",
+    titulo: "La teoria del amor",
+    imagen: "img/Libros/romance/la teoria del amor.avif",
+    categoria: {
+     nombre: "Romance",
+        id: "romance"
+    },
+},
+{
+    id: "Romance-04",
+    titulo: "Rivales divinos",
+    imagen: "img/Libros/romance/rivales-divinos.avif",
+    categoria: {
+     nombre: "Romance",
+        id: "romance"
+    },
+},
+{
+    id: "Romance-05",
+    titulo: "Romper el hielo",
+    imagen: "img/Libros/romance/romper-el-hielo.avif",
+    categoria: {
+     nombre: "Romance",
+        id: "romance"
+    },
+}
+];
